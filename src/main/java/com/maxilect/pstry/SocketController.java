bca6ffe089ccf37900fcb970313bc0181dc89391
omaximchuk <omaximchuk@gmail.com>
2016-12-08 09:25:28 +0300

    begin db change notification impl

--- src/main/java/com/maxilect/pstry/SocketController.java
+++ src/main/java/com/maxilect/pstry/SocketController.java
@@ -1,15 +1,13 @@
 package com.maxilect.pstry;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
-
-
 import com.maxilect.pstry.dao.ResultMapper;
 import com.maxilect.pstry.dao.TaskMapper;
 import oracle.jdbc.OracleConnection;
+import oracle.jdbc.dcn.DatabaseChangeEvent;
+import oracle.jdbc.dcn.DatabaseChangeListener;
+import oracle.jdbc.dcn.DatabaseChangeRegistration;
 import org.apache.commons.dbcp.BasicDataSource;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -18,54 +16,80 @@ import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.apache.log4j.Logger;
+
+import javax.annotation.PostConstruct;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Random;
 
 @Controller
 public class SocketController {
 
-  @Autowired private SimpMessagingTemplate template;  
-  private TaskScheduler scheduler = new ConcurrentTaskScheduler();
-  private List<Task> tasks = new ArrayList<Task>();
-  private Random rand = new Random(System.currentTimeMillis());
-  final static Logger logger = Logger.getLogger(SocketController.class);
+    final static Logger logger = Logger.getLogger(SocketController.class);
+    @Autowired
+    private SimpMessagingTemplate template;
+    private TaskScheduler scheduler = new ConcurrentTaskScheduler();
+    private List<Task> tasks = new ArrayList<Task>();
+    private Random rand = new Random(System.currentTimeMillis());
+    @Autowired
+    private BasicDataSource ds;
 
-  @Autowired
-  private BasicDataSource ds;
+    @Autowired
+    private TaskMapper taskMapper;
+    @Autowired
+    private ResultMapper resultMapper;
 
-  @Autowired
-  private TaskMapper taskMapper;
-  @Autowired
-  private ResultMapper resultMapper;
+    private void updateTaskAndBroadcast() {
+        template.convertAndSend("/topic/tasks", tasks);
+    }
 
-  private void updateTaskAndBroadcast() {
-    template.convertAndSend("/topic/tasks", tasks);
-  }
-
-  /**
-   * Handler to add one task
-   */
-  @MessageMapping("/addTask")
-  public void addStock(Task task) throws Exception {
-    //TODO add proper logic
+    /**
+     * Handler to add one task
+     */
+    @MessageMapping("/addTask")
+    public void addStock(Task task) throws Exception {
+        //TODO add proper logic
 //    tasks.add(new Task(1l, "taskvalue", new Date(), new Date()));
-    tasks = taskMapper.getLatestTasks(7);
-    updateTaskAndBroadcast();
-  }
+        tasks = taskMapper.getLatestTasks(7);
+        updateTaskAndBroadcast();
+    }
 
-  
-  /**
-   * Serve the main page
-   */
-  @RequestMapping(value = "/", method = RequestMethod.GET)
-  public String home() {
-    return "home";
-  }
 
-  public void registerNotification(){
-      String url = ds.getUrl();
-      String user = ds.getUsername();
-      String pwd = ds.getPassword();
+    /**
+     * Serve the main page
+     */
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String home() {
+        return "home";
+    }
 
-  }
+    @PostConstruct
+    public void registerNotification() throws SQLException {
+        OracleConnection connection = null;
+        DatabaseChangeRegistration changeRegistration = null;
+        try {
+            connection = (OracleConnection) ds.getConnection();
+            changeRegistration = connection.registerDatabaseChangeNotification(buildProperties());
+            changeRegistration.addListener(new DatabaseChangeListener() {
+                @Override
+                public void onDatabaseChangeNotification(DatabaseChangeEvent databaseChangeEvent) {
+                    //do main staff here
+                }
+            });
+        } catch (SQLException e) {
+            if (connection != null)
+                connection.unregisterDatabaseChangeNotification(changeRegistration);
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Properties buildProperties() {
+        Properties properties = new Properties();
+        properties.setProperty(OracleConnection.DCN_NOTIFY_ROWIDS, "true");
+        properties.setProperty(OracleConnection.DCN_QUERY_CHANGE_NOTIFICATION, "true");
+        return properties;
+    }
 
 }
